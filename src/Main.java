8a761f3185a755a25f47a730cca28fee93d742c0
Zavodilin1985 <zavodilin-ru@yandex.ru>
2023-08-14 15:56:54 +0300

    first commit feature_1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
 
+    public static int numA (int a) {
+
+        return a + 10;
+    }
+
+    public static int numB (int b) {
+
+        System.out.println(b);
+        return b - 5;
+    }
+
     public static void main(String[] args) {
 
-        int a = 10;
-        int b = 20;
+
+        int c = 40;
+        int a = numA(10);
+        int b = numB(20);
         System.out.println(b-a);
     }
 }
